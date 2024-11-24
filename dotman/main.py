7b448f00bc64d28dd7e7e1bc58969db6b88a24a5
allavinner <joel.w.ottosson@gmail.com>
2024-11-24 21:31:35 +0100

    FEAT: Changed from dotman folder to dotman file for config.

--- dotman/main.py
+++ dotman/main.py
@@ -27,20 +27,15 @@ class Project:
     path: Path
     config: Config
     _home = Path.home()
-    _dotman_dir = ".dotman"
-    _config_file = "config.json"
+    _config_file = ".dotman.json"
 
     @property
     def config_path(self) -> Path:
-        return Path(self._dotman_dir, self._config_file)
+        return Path(self._config_file)
 
     @property
     def full_config_path(self) -> Path:
         return Path(self.path, self.config_path)
-
-    @property
-    def full_dotman_path(self) -> Path:
-        return Path(self.path, self._dotman_dir)
 
     @classmethod
     def init(cls, project_path: Path) -> Self:
@@ -50,10 +45,8 @@ class Project:
             project_path.mkdir(parents=True)
         config = Config(links={})
         self = cls(path=project_path, config=config)
-        dotman_path = Path(project_path, cls._dotman_dir)
-        if dotman_path.exists():
+        if Path(project_path, cls._config_file).exists():
             raise ProjectException("Given path is already a dotman project.")
-        dotman_path.mkdir()
         self.write()
         return self
 
@@ -61,7 +54,7 @@ class Project:
     def from_path(cls, project_path: Path) -> Self:
         if not project_path.is_dir():
             raise ProjectException("Given project path is not a directory")
-        config_file = Path(project_path, cls._dotman_dir, cls._config_file)
+        config_file = Path(project_path, cls._config_file)
         if not config_file.is_file():
             raise ProjectException("Cannot find config in project")
         with open(config_file, "r") as f:
@@ -70,7 +63,7 @@ class Project:
         return cls(path=project_path, config=config)
 
     def write(self):
-        with open(Path(self.path, self._dotman_dir, self._config_file), "w") as f:
+        with open(Path(self.path, self._config_file), "w") as f:
             f.write(self.config.model_dump_json())
 
     def add_link(self, source: Path, target_name: str | None = None) -> None:
@@ -86,7 +79,7 @@ class Project:
         if not source.exists():
             raise ProjectException(f"Source {source} does not exists.")
         try:
-            home_to_source_path = source.relative_to(self._home)
+            home_to_source_path = source.absolute().relative_to(self._home)
         except ValueError:
             raise ProjectException("Source must be relative to home.")
 
@@ -100,9 +93,14 @@ class Project:
         if link_name not in self.config.links:
             raise ProjectException(f"Could not find link 'link_name' in project.")
         link = self.config.links[link_name]
-        if link.source.exists() or link.source.is_symlink():
-            raise ProjectException(f"Cannot restore link. Alrady file in links source.")
-        Path(self._home, link.source).symlink_to(Path(self.path, link_name).absolute())
+        full_link_path = Path(self._home, link.source)
+        if full_link_path.exists() or full_link_path.is_symlink():
+            raise ProjectException(
+                f"Cannot restore link. Already file at links source."
+            )
+        if not full_link_path.parent.exists():
+            full_link_path.parent.mkdir(parents=True)
+        full_link_path.symlink_to(Path(self.path, link_name).absolute())
 
     def restore(self):
         for link_name in self.config.links:
